2bc8e3137f463d7783fa0298e4e616fe7ed40490
Evgeny Rybakov <erybakov@exadel.com>
2020-04-22 21:41:39 +0300

    add firebase cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { CallableContext } from "firebase-functions/lib/providers/https";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send("Hello from Firebase1y!");
+});
+
+admin.initializeApp();
+export const addAdminRole = functions.https.onCall((data, context: CallableContext) => {
+  //get user and add custom claims
+  return admin.auth().getUserByPhoneNumber(data.phoneNumber).then(user => {
+    return admin.auth().setCustomUserClaims(user.uid, {
+      admin: true
+    });
+  }).then(() => {
+    return {
+      message: `Success${data.phoneNumber} has been made an admin`
+    };
+  }).catch(err => {
+    return err;
+  });
 });
